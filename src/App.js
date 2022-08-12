f8fd80b0a3a527480c87321cd011843dce518756
MirHeyder03 <mhyahyayev03@gmail.com>
2022-08-12 19:11:09 +0400

    updated

--- src/App.js
+++ src/App.js
@@ -1,49 +1,58 @@
-import { Routes,Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import axios from "axios";
 import React, { useEffect, useState } from "react";
 import Navbar from "./components/Navbar";
 import SearcBar from "./components/SearchBar";
 import Users from "./components/Users";
-import { CSSProperties } from "react";
 import MoonLoader from "react-spinners/MoonLoader";
 import UserDetailPage from "./components/Users/UserDetailPage";
+const URL = "https://api.github.com/users";
 
-function App() {
-  const [user,setUser]=([]);
+const App = (props) => {
   const [users, setUsers] = useState([]);
-  const [loading,setLoading]=useState(false);
+  const [loading, setLoading] = useState(true);
   const cssOverride = {
     display: "block",
     margin: "100px auto",
     borderColor: "red",
-    
   };
   useEffect(() => {
-    setTimeout(() => {
-      const url = "https://api.github.com/users";
-      axios.get(url).then(({ data }) => setUsers(data));
-    }, []);
-  }, 3000);
+    axios.get(URL).then(({ data }) => setUsers(data));
+  }, []);
   const searchUser = (search) => {
     const url = `https://api.github.com/search/users?q=${search}`;
     axios.get(url).then(({ data }) => setUsers(data.items));
   };
-  const getUser=(username)=>{
-    const url = `https://api.github.com/users/${username}`;
-    axios.get(url).then(({ data }) => setUser(data));
-  }
+  const getUser = (username) => {
+    console.log(username)
+    
+  };
+  getUser()
   return (
     <div>
       <Navbar />
       <Routes>
-        <Route path="/" element={<SearcBar searchUser={searchUser} />}/>
-        <Route path="/user/:login" element={<UserDetailPage getUser={getUser} user={user} setUser={setUser}/>}/>
-        </Routes>
-      {
-        loading ? <MoonLoader loading={loading} cssOverride={cssOverride} size={150} /> : <Users users={users} />
-      }
+        <Route path="/" element={<SearcBar searchUser={searchUser} />} />
+        <Route
+          exact
+          path={"/user/:login"}
+          element={<UserDetailPage />}
+        />
+        <Route
+          path="/"
+          element={
+            <MoonLoader
+              loading={loading}
+              cssOverride={cssOverride}
+              size={150}
+            />
+          }
+        />
+      </Routes>
+      {/* <MoonLoader loading={loading} cssOverride={cssOverride} size={150}/> */}
+      <Users users={users} />
     </div>
   );
-}
+};
 
 export default App;
--- src/components/Users/UserDetailPage/index.jsx
+++ src/components/Users/UserDetailPage/index.jsx
@@ -1,15 +1,57 @@
-import React from 'react'
-import { useEffect } from 'react';
+import axios from "axios";
+import React, { useState } from "react";
+import { useEffect } from "react";
+import { useParams } from "react-router-dom";
 
-const UserDetailPage = ({getuser,user,login},props) => {
-    // useEffect(()=>{
-    //   getuser(props.match.params.login)
-    // },[])
+const UserDetailPage = () => {
+  const [user, setUser] = useState({});
+
+  console.log(user);
+  let { login } = useParams();
+
+  useEffect(() => {
+    const url = `https://api.github.com/users/${login}`;
+    axios.get(url).then(({ data }) => setUser(data));
+  }, [login]);
   return (
-    <div>
-      <h1>{login}</h1>
+    <div className="container mt-3">
+      <div className="row">
+        <div className="col-md-3">
+          <img url={""} className="card-img-top" />
+          <div className="card-body">
+            <p className="card-text">{login}</p>
+            <p>
+              <i className="fas fa-map-marker-alt"></i>
+            </p>
+            <p className="btn btn-block btn-dark btn-sm">Github Profile</p>
+          </div>
+        </div>
+        <div className="col-md-9">
+          <div className="card">
+            <div className="card-body">
+              {
+                <>
+                  <h3>About</h3>
+                  <p>bio</p>
+                </>
+              }
+              {
+                <>
+                  <h3>blog</h3>
+                  <p>blog</p>
+                </>
+              }
+              <div>
+                <span className="badge badge-primary m-1">Followers :</span>
+                <span className="badge badge-danger m-1">FolloWING :</span>
+                <span className="badge badge-success m-1">Repos :</span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export default UserDetailPage;
